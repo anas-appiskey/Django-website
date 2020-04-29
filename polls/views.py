c505ecfd4bb58d410012d00ee3c1fd94222beadf
anasali2193 <anasali2193@gmail.com>
2020-04-30 00:51:06 +0500

    update views

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse , Http404
 
 from .models import Question
@@ -21,11 +21,14 @@ def index(request):
 
 #in this case the id is the PK
 def details(request, question_id):
-    try:
-        question = Question.objects.get(pk = question_id)
-    except Question.DoesNotExists:
-        raise Http404("question does not match")
-    return HttpResponse("you are looking at question %s." %question_id)
+    # try:
+    #     question = Question.objects.get(pk = question_id)
+    # except Question.DoesNotExists:
+    #     raise Http404("question does not match")
+   
+    question = get_object_or_404(Question ,pk = question_id)
+
+    return render(request , 'polls/details.html', {'question': question})
 
 def results(request, question_id):
     response = "you are looking at results of qstn %s. "
