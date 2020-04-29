c21eb072c6cf01b4e0a34f3a19d094f0649322c4
anasali2193 <anasali2193@gmail.com>
2020-04-29 20:18:52 +0500

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 # Create your views here.
@@ -10,8 +11,11 @@ from .models import Question
 #this function will display whatever in the index()
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list' : latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 #in this case the id is the PK
 def details(request, question_id):
