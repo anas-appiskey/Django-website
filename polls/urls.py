bde5e73643af88cfecad5cbda3daf16f853aa329
anasali2193 <anasali2193@gmail.com>
2020-04-30 18:14:02 +0500

    Update Generic View

--- polls/urls.py
+++ polls/urls.py
@@ -5,12 +5,13 @@ app_name = 'polls'
 urlpatterns = [
     #it displays the views file function index 
    # the 'name' value as called by the {% url %} template tag
-    path('',views.index, name = "index"),
+    path('',views.IndexView.as_view(), name = "index"),
     #ex : polls/5
-    path('<int:question_id>/',views.details, name = "details"),
+    #convert code to generic view
+    path('<int:pk>/',views.DetailsView.as_view(), name = "details"),
     
     #ex : polls/5/results
-    path('<int:question_id>/results',views.results, name = "results"),
+    path('<int:pk>/results',views.ResultsView.as_view(), name = "results"),
    
     #ex : polls/5/vote
      path('<int:question_id>/vote',views.vote, name = "vote"),
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse , HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 # Create your views here.
@@ -9,33 +10,49 @@ from .models import Question, Choice
 #request is pass whenever you do anything
 
 #this function will display whatever in the index()
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+class IndexView(generic.ListView):
     
-    #this context pass the variable latest_qstn_list to the template 
-    # index.html file throught render 
-    #the thing we are accessing is the context dict key 
-    context = {
-        'latest_question_list' : latest_question_list,
-    }
-    return render(request ,'polls/index.html', context)
+    template_name = 'polls/index.html'
+    #you can change this question_list by using this variable
+    context_object_name = 'latest_question_list'
 
-#in this case the id is the PK
-def details(request, question_id):
+    def get_queryset(self):
+        """Return the last five pub"""
+        return Question.objects.order_by('-pub_date')[:5]
+
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    
+    # #this context pass the variable latest_qstn_list to the template 
+    # # index.html file throught render 
+    # #the thing we are accessing is the context dict key 
+    # context = {
+    #     'latest_question_list' : latest_question_list,
+    # }
+    # return render(request ,'polls/index.html', context)
+    
+"""in this case the id is the PK"""
+
+class DetailsView(generic.DetailView):
     # try:
     #     question = Question.objects.get(pk = question_id)
     # except Question.DoesNotExist:
     #     raise Http404("question does not match")
    
-    question = get_object_or_404(Question ,pk = question_id)
-    return render(request , 'polls/details.html', {'question': question})
+    # question = get_object_or_404(Question ,pk = question_id)
+    # return render(request , 'polls/details.html', {'question': question})
+    model = Question
+    template_name = 'polls/details.html'
 
-def results(request, question_id):
-    question = get_object_or_404(Question , pk = question_id)
 
-    return render(request , 'polls/results.html',{'question' : question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+    # question = get_object_or_404(Question , pk = question_id)
+    # return render(request , 'polls/results.html',{'question' : question})
 
 def vote(request, question_id):
+
     question = get_object_or_404(Question, pk= question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
