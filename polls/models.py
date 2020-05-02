6d6c0c2df76b282d158671e7d92364a376c15693
anasali2193 <anasali2193@gmail.com>
2020-05-02 10:40:26 +0500

    Add Test Functions

--- polls/models.py
+++ polls/models.py
@@ -2,14 +2,15 @@ from django.db import models
 import datetime
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    
-    #it will show the text not only the number of current item 
+
+    # it will show the text not only the number of current item
     def __str__(self):
         return self.question_text
-    
+
     def was_published_recently(self):
         # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
         now = timezone.now()
@@ -18,10 +19,10 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    #This is a foreign key and django is handeling the PK itself
+    # This is a foreign key and django is handeling the PK itself
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0) 
+    votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,48 +1,58 @@
-from django.shortcuts import render , get_object_or_404
-from django.http import HttpResponse , HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
+
+
 # Create your views here.
 
-#note 
-#request is pass whenever you do anything
+# note
+# request is pass whenever you do anything
 
-#this function will display whatever in the index()
+# this function will display whatever in the index()
 class IndexView(generic.ListView):
-    
     template_name = 'polls/index.html'
-    #you can change this question_list by using this variable
-    
+    # you can change this question_list by using this variable
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         """Return the last five pub"""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    
-    # #this context pass the variable latest_qstn_list to the template 
-    # # index.html file throught render 
-    # #the thing we are accessing is the context dict key 
+
+    # #this context pass the variable latest_qstn_list to the template
+    # # index.html file throught render
+    # #the thing we are accessing is the context dict key
     # context = {
     #     'latest_question_list' : latest_question_list,
     # }
     # return render(request ,'polls/index.html', context)
-    
+
+
 """in this case the id is the PK"""
+
 
 class DetailsView(generic.DetailView):
     # try:
     #     question = Question.objects.get(pk = question_id)
     # except Question.DoesNotExist:
     #     raise Http404("question does not match")
-   
+
     # question = get_object_or_404(Question ,pk = question_id)
     # return render(request , 'polls/details.html', {'question': question})
     model = Question
     template_name = 'polls/details.html'
+
+    def get_queryset(self):
+        """
+              Excludes any questions that aren't published yet.
+              """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 
 class ResultsView(generic.DetailView):
@@ -52,43 +62,26 @@ class ResultsView(generic.DetailView):
     # question = get_object_or_404(Question , pk = question_id)
     # return render(request , 'polls/results.html',{'question' : question})
 
-def vote(request, question_id):
 
-    question = get_object_or_404(Question, pk= question_id)
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    
-    #doesnotexist not have exists
+
+    # doesnotexist not have exists
     except (KeyError, Choice.DoesNotExist):
-        #Redisplay the question voting form
-        return render(request, 'polls/details.html',{
-            'question' : question,
-            'error_message' :"you didn't select a choice ",
+        # Redisplay the question voting form
+        return render(request, 'polls/details.html', {
+            'question': question,
+            'error_message': "you didn't select a choice ",
         })
     else:
-        selected_choice.votes +=1
+        selected_choice.votes += 1
         selected_choice.save()
-    
+
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        
-        
-        #2.13.58
-        return HttpResponseRedirect(reverse('polls:results', args =(question.id,)))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # 2.13.58
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
